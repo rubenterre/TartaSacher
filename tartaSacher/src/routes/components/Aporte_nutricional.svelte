144ed6d6857f35bd05b19abcaa06f5e825c96001
PiliGMinem <mariadelpilargarciamirasinem@gmail.com>
2024-01-18 11:14:02 +0100

    commit initial

---
+++ tartaSacher/src/routes/components/Aporte_nutricional.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+<p>Qué me aporta esta receta…</p>
+<input type="text" placeholder="Información nutricional" required>
+<button>+</button>
+<style>
+    :global(p){
+        color: #000;
+        font-family: Nunito;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+    }
+</style>
