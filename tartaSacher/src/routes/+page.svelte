0e8aacab725aee88db72e27f2bc33d90b557d4e9
MikeNeira <miguelneira827@gmail.com>
2024-01-18 13:42:20 +0100

    Actualización de componente Video

--- tartaSacher/src/routes/+page.svelte
+++ tartaSacher/src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+  import Video from "./components/Video.svelte";
+      </script>
+      
+      <section>
+          <div class="cabecera">
+              <div class="cabecera__video">
+              <Video/>
+              </div>
+              <div class="cabecera__detalle">
+                  
+              </div>
+          </div>
+          
+      </section>
+      
+      <style>
+        .cabecera {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          background-color: var(--cor-principal);
+          margin-top: 30px;
+        }
+      
+        .cabecera__video {
+          position: relative;
+          width: 100%;
+          padding-bottom: 56.25%;
+        }
+      
+        .cabecera__detalle{
+          padding: 50px;
+        }
+      </style>
+      
