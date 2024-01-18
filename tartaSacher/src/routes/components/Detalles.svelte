a3cc5564ecbf0a4d4c38f56c625caef02d22a455
Xoan Carreira <xoancano@gmail.com>
2024-01-18 11:14:02 +0100

    commit initial Inicio creación compoñente Detalles

---
+++ tartaSacher/src/routes/components/Detalles.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<div class="detalles">
+    <div class="detalles__left">
+        <p class="dificultade"><span class="icono"><img src="dificultad_icono.webp" alt="Logo de dificultade da receita"></span>Media</p>
+        <p class="preparacion"><span class="icono"><img src="tiempo_icono.webp" alt="Logo de tempo de preparación da receita"></span>30 min. de preparación</p>
+    </div>
+    <div class="detalles__right">
+        <p class="personas"><span class="icono"><img src="personas_icono.webp" alt="Logo de para cantas persoas é a receita"></span>10-12 personas</p>
+        <p class="cocion"><span class="icono"><img src="coccion_icono.webp" alt="Logo de tempo de cocción da receita"></span>50 min. de cocción</p>
+    </div>
+</div>
+
+<style></style>
