b22af1395299464ca62d8e3674745ba188623022
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2024-01-18 10:10:19 +0100

    commit initial

---
+++ tartaSacher/src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+
+<slot/>
+
+<style>
+
+@font-face{
+    font-family: Nunito;
+    src: url(font/Nunito-VariableFont_wght.ttf);
+}
+
+:root{
+    --cor-principal: #979797;
+    -cor-secundario: #343239;
+    --fonte-principal: Nunito;
+}
+
+:global(*){
+    margin:0px;
+    padding: 0px;
+    box-sizing: border-box;
+}
+
+:global(body){
+    background: white;
+    font-size: 16px;
+}
+
+</style>
